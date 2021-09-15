d2aae59a44fe5843d3d0fcfb7d117d411db02273
Lforwhat <1277149488@qq.com>
2021-09-15 08:00:06 +0800

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+let data;
+let mapData;
+// $(document).ready(function(){
+//     $.getJSON("", function(json){
+//         data = json;
+//         console.log(data);
+//     });
+//     mapData = Highcharts.geojson(Highcharts.maps['custom/world']);
+//     $.each(mapData, function () {
+// 		this.id = this.properties['hc-key']; // for Chart.get()
+// 		this.flag = this.id.replace('UK', 'GB').toLowerCase();
+// 	});
+//     console.log(mapData);
+// });
